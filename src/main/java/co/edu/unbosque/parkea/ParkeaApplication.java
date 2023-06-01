2160c21859a04efc550d01ce551b6e932d9584d2
Mejooo08 <73041626+Mejooo08@users.noreply.github.com>
2023-06-01 13:01:58 -0500

    pom

--- src/main/java/co/edu/unbosque/parkea/ParkeaApplication.java
+++ src/main/java/co/edu/unbosque/parkea/ParkeaApplication.java
@@ -2,9 +2,17 @@ package co.edu.unbosque.parkea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkeaApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkeaApplication.class, args);
