d20d7a234bcd532c511c1d7f2948c84913f48d34
Estefa0399 <74272847+Estefa0399@users.noreply.github.com>
2023-06-03 11:03:22 -0500

    reporte reserva

--- src/main/java/co/edu/unbosque/parkea/service/ReservaCupoServiceAPI.java
+++ src/main/java/co/edu/unbosque/parkea/service/ReservaCupoServiceAPI.java
@@ -2,10 +2,16 @@ package co.edu.unbosque.parkea.service;
 
 import co.edu.unbosque.parkea.commons.GenericServiceAPI;
 import co.edu.unbosque.parkea.model.ReservaCupo;
+import net.sf.jasperreports.engine.JRException;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public interface ReservaCupoServiceAPI extends GenericServiceAPI<ReservaCupo, Integer> {
 
     ArrayList<Integer> facturacion(int tarifa, String horaIngreso, String horaSalida, String fidelizacion);
+
+    byte[] exportPdf() throws JRException, FileNotFoundException;
+
+    byte[] exportXls() throws JRException, FileNotFoundException;
 }
